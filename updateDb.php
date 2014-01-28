a7eec837c6404e8f775650146e5bdb7553da3c0c
Bawaaaaah <limason51@gmail.com>
2014-01-28 12:46:52 +0100

    Revert "ading file to update the sqlite db"
    This reverts commit 09d6e55a3fbe8242e0fb591db906383d55de1ead.

--- updateDb.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-$dbname = 'snippets.sqlite';
-$mytable = "snippets";
-$base = new SQLite3($dbname);
-$query_name = "ALTER TABLE $mytable ADD COLUMN 'highlight' longtext, 'lines' integer";
-$base->query($query_name);
-?>
